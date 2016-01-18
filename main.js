44475583d1dc6df9ea374269e6c3a6bf12d9256f
magemore <alex@magemore.com>
2016-01-19 00:55:13 +0200

    Create main.js
    Find images and remove them if width == 782. Also remove all iframes.

---
+++ main.js
@@ -0,0 +1,22 @@
+function removeAds() {
+	var list = document.getElementsByTagName("img");
+	for (i = 0; i < list.length; i++) {
+		w = parseInt(list[i].getAttribute('width'));
+		if (w==728) {
+			if (list[i].parentNode.nodeName=='A') {
+				list[i].parentNode.outerHTML='';
+			}
+			else {
+				list[i].outerHTML='';
+			}
+		}
+	}
+	
+	var list = document.getElementsByTagName("iframe");
+	for (i = 0; i < list.length; i++) {	
+		list[i].outerHTML='';
+	}
+}
+
+removeAds();
+setInterval(removeAds,100);
